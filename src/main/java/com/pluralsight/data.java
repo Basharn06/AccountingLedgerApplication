102984f8fe17d5f885280ad6fa004f175483e724
basharnasser <bnasser469@gmail.com>
2025-10-10 11:25:04 -0400

    commit

--- src/main/java/com/pluralsight/data.java
+++ src/main/java/com/pluralsight/data.java
@@ -1,4 +1,13 @@
 package com.pluralsight;
 
+import java.math.BigDecimal;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
 public class data {
-}
+    // private fields
+    private LocalDate date;
+    private LocalTime time;
+    private String description;
+    private String vendor;
+    private BigDecimal amount;
